5f4a7a5430e925500e711422fb4b2e594bb87f1d
nrgupta <30555151+nrgupta@users.noreply.github.com>
2019-10-03 11:08:06 -0500

    i changed queue demo

--- QueueDemo.java
+++ QueueDemo.java
@@ -2,14 +2,15 @@ public class QueueDemo
 {
    public static void main(String[] args)
    {
-      CircularArrayQueue q = new CircularArrayQueue();
-      
-      System.out.println("test"); 
+      Queue q = new Queue();
+
+      System.out.println("test");
       //add to q
 
       while (!q.empty())
       {
          //remove from q
+         System.out.println("hahahahahahahahaahahahahah");
       }
    }
 }
